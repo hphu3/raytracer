1ae919671799bd91d81eb753c2cdb6963d8d7be3
Henry Phu <henry@inventables.com>
2022-10-23 16:52:54 -0500

    smaller clock

--- src/clock.ts
+++ src/clock.ts
@@ -3,17 +3,17 @@ const { Matrix } = require('./matrix.ts');
 const { Canvas } = require('./canvas.ts');
 const fs = require('fs');
 
-const canvas = new Canvas(600, 600);
+const canvas = new Canvas(300, 300);
 
-const color = new Color(1, 1, 1);
+const color = new Color(1, 0.5, 1);
 
 const twelveOClock = new Point(0, 1, 0);
 const points = [];
 
 for (let i=0; i < 12; i++){
   const rotation = Matrix.rotationZ(Math.PI * i / 6);
-  const scale = Matrix.scaling(250, 250, 250);
-  const translate = Matrix.translation(300, 300, 0);
+  const scale = Matrix.scaling(100, 100, 100);
+  const translate = Matrix.translation(150, 150, 0);
   const transform = translate.multiply(scale).multiply(rotation);
   const clockHour = transform.multiply(twelveOClock);
   points.push(clockHour);
