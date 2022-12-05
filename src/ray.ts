c5aba3c8e4811bc84cb8e53cb29e6a810bbd0813
Henry Phu <henry@inventables.com>
2022-12-04 21:31:21 -0600

    raytracing a sphere

--- src/ray.ts
+++ src/ray.ts
@@ -19,7 +19,6 @@ class Ray {
   intersects(sphere: typeof Sphere) {
     // use inversely transformed ray in intersection calc to account for sphere transforms
     const rayToCast = this.transform(sphere.transform.invert());
-    console.log(rayToCast)
     const sphereToRayTuple = rayToCast.origin.subtract(sphere.center)
     const sphereToRay = new Vector(sphereToRayTuple.x, sphereToRayTuple.y, sphereToRayTuple.z, sphereToRayTuple.w);
     const a = rayToCast.direction.dot(rayToCast.direction);
@@ -27,12 +26,10 @@ class Ray {
     const c = sphereToRay.dot(sphereToRay) - 1;
 
     const discriminant = Math.pow(b, 2) - 4 * a * c;
-    console.log(discriminant)
     if (discriminant < 0) { return new Intersections() }
     const t1 = (-b - Math.sqrt(discriminant)) / (2 * a);
     const t2 = (-b + Math.sqrt(discriminant)) / (2 * a);
 
-    console.log(t1);
     return new Intersections(new Intersection(t1, sphere), new Intersection(t2, sphere));
   }
 
---
+++ src/spherecast.ts
@@ -0,0 +1,33 @@
+const { Tuple, Point, Vector, Color } = require('./tuple.ts');
+const { Matrix } = require('./matrix.ts');
+const { Intersection, Intersections } = require('./intersection.ts');
+const { Canvas } = require('./canvas.ts');
+const { Sphere } = require('./sphere.ts');
+const { Ray } = require('./ray.ts');
+const fs = require('fs');
+
+const canvas = new Canvas(300, 300);
+const color = new Color(1, 0.5, 0.5);
+const sphere = new Sphere();
+sphere.setTransform(
+  Matrix.translation(canvas.width / 2, canvas.height / 2, 0).multiply(Matrix.scaling(100, 100, 1))
+);
+const cameraOrigin = new Point(canvas.width / 2, canvas.height / 2, -10);
+
+for (let x=0; x<canvas.width; x++) {
+  for (let y=0; y<canvas.height; y++) {
+    let direction = new Point(x, y, 0).subtract(cameraOrigin);
+    const r = new Ray(cameraOrigin, direction);
+    const xs = r.intersects(sphere);
+    const intersectionHit = xs.hit();
+    if (intersectionHit != null) {
+      canvas.writePixel(Math.round(x), Math.round(y), color)
+    }
+  }
+}
+
+
+fs.writeFile('sphere.ppm', canvas.toPPM(), function (err: any) {
+  if (err) throw err;
+  console.log('Done');
+});
