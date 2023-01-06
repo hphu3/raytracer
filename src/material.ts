1dd3aa4d607312607cf6fa9f740bc7742691c0a4
Henry Phu <henry@inventables.com>
2023-01-06 13:00:53 -0600

    material lighting

--- src/material.ts
+++ src/material.ts
@@ -1,14 +1,15 @@
-const { Color } = require('./tuple');
+const { Tuple, Vector, Point, Color } = require('./tuple');
+const { PointLight } = require('./point_light')
 
 class Material {
-  ambient: Number
-  diffuse: Number
-  specular: Number
-  shininess: Number
+  ambient: number
+  diffuse: number
+  specular: number
+  shininess: number
   color: typeof Color
 
 
-  constructor(color: typeof Color, ambient: Number, diffuse: Number, specular: Number, shininess: Number) {
+  constructor(color: typeof Color, ambient: number, diffuse: number, specular: number, shininess: number) {
     this.ambient = ambient || 0.1;
     this.diffuse = diffuse || 0.9;
     this.specular = specular || 0.9;
@@ -16,12 +17,39 @@ class Material {
     this.color = color || new Color(1, 1, 1);
   }
 
+  lighting(light: typeof PointLight, point: typeof Point, eyev: typeof Vector, normalv: typeof Vector) {
+    const effectiveColor = this.color.multiply(light.intensity);
+    const lightv = Tuple.asPointOrVector(...light.position.subtract(point).tuple).normalize();
+    const ambient = effectiveColor.multiply(this.ambient);
+
+    let diffuse;
+    let specular;
+
+    const lightDotNormal = lightv.dot(normalv);
+    if (lightDotNormal < 0) {
+      diffuse = new Color(0, 0, 0);
+      specular = new Color(0, 0, 0);
+    } else {
+      diffuse = effectiveColor.multiply(this.diffuse).multiply(lightDotNormal);
+      const reflectv = Tuple.asPointOrVector(...lightv.negate().tuple).reflect(normalv);
+      const reflectDotEye = Tuple.asPointOrVector(...reflectv.tuple).dot(eyev);
+
+      if (reflectDotEye <= 0) {
+        specular = new Color(0, 0, 0);
+      } else {
+        const factor = Math.pow(reflectDotEye, this.shininess);
+        specular = light.intensity.multiply(this.specular).multiply(factor);
+      }
+    }
+    return ambient.add(diffuse).add(specular);
+  }
+
   equals(m2: Material) {
     return this.ambient == m2.ambient &&
-    this.diffuse == m2.diffuse &&
-    this.specular == m2.specular &&
-    this.shininess == m2.shininess &&
-    this.color.equals(m2.color);
+      this.diffuse == m2.diffuse &&
+      this.specular == m2.specular &&
+      this.shininess == m2.shininess &&
+      this.color.equals(m2.color);
   }
 }
 
---
+++ test/material.test.js
@@ -0,0 +1,45 @@
+const { Color, Point, Vector } = require('../src/tuple');
+const { Material } = require('../src/material');
+const { PointLight } = require('../src/point_light');
+
+describe("material", () => {
+  let material;
+  let position;
+
+  beforeEach(() => {
+    material = new Material();
+    position = new Point(0, 0, 0);
+  });
+
+  test("lighting with the eye between the light and the surface", () => {
+    const eyev = new Vector(0, 0, -1);
+    const normalv = new Vector(0, 0, -1);
+    const light = new PointLight(new Point(0, 0, -10), new Color(1, 1, 1));
+    const result = material.lighting(light, position, eyev, normalv);
+    expect(result.equals(new Color(1.9, 1.9, 1.9))).toBe(true);
+  });
+
+  test("lighting with the eye between the light and the surface", () => {
+    const eyev = new Vector(0, Math.sqrt(2) / 2, Math.sqrt(2) / 2);
+    const normalv = new Vector(0, 0, -1);
+    const light = new PointLight(new Point(0, 0, -10), new Color(1, 1, 1));
+    const result = material.lighting(light, position, eyev, normalv);
+    expect(result.equals(new Color(1, 1, 1))).toBe(true);
+  });
+
+  test("lighting with the eye opposite the surface, light offset 45deg", () => {
+    const eyev = new Vector(0, 0, -1);
+    const normalv = new Vector(0, 0, -1);
+    const light = new PointLight(new Point(0, 0, -10), new Color(1, 1, 1));
+    const result = material.lighting(light, position, eyev, normalv);
+    expect(result.equals(new Color(0.7364, 0.7364, 0.7364))).toBe(true);
+  });
+
+  test("lighting with the eye in the path of the reflection vector", () => {
+    const eyev = new Vector(0, -Math.sqrt(2) / 2, -Math.sqrt(2) / 2);
+    const normalv = new Vector(0, 0, -1);
+    const light = new PointLight(new Point(0, 0, 10), new Color(1, 1, 1));
+    const result = material.lighting(light, position, eyev, normalv);
+    expect(result.equals(new Color(0.1, 0.1, 0.1))).toBe(true);
+  });
+});
